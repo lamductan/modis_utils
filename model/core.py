16e483deb9f4887c281425156bb92097cd657771
lamductan <lamductan@gmail.com>
2018-12-30 23:17:14 +0700

    change model

--- model/core.py
+++ model/core.py
@@ -495,11 +495,7 @@ def _create_model_with_tensorflow_2(model_params, compile_params):
                            bias_constraint=bias_constraint)(batchNorm_layers[-1])
 
     def myFunc(x):
-        with open('log.txt', 'a') as f:
-            f.write('x.shape = ')
-            f.write(str(x.shape))
-            f.write('\n')
-        f.close()
+        tf.Print('x.shape = ', x.shape)
         x = scale_data_tf(x)
         x = tf.reshape(x, [batch_size, input_shape[1], input_shape[2]])
         list_x = tf.split(x, batch_size)
