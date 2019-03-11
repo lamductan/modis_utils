34dde6e21c1d6e40a742088da1c68181d18701b7
lamductan <lamductan@gmail.com>
2019-03-11 15:18:08 +0700

    add vgg16

--- model/model_utils_factory.py
+++ model/model_utils_factory.py
@@ -2,6 +2,7 @@ from modis_utils.model.convlstm_simple import ConvLSTMSimpleOneTimeStepsOutput
 from modis_utils.model.convlstm_simple import ConvLSTMSimpleSequenceTimeStepsOutput
 from modis_utils.model.cplx_model import SkipConvLSTMSingleOutput
 from modis_utils.model.resnet_lstm import ResnetLSTMSingleOutput
+from modis_utils.model.vgg16_lstm import Vgg16LSTMSingleOutput
 
 
 def get_model_utils(model_name, output_timesteps):
@@ -14,3 +15,5 @@ def get_model_utils(model_name, output_timesteps):
         return SkipConvLSTMSingleOutput
     elif model_name == 'resnet_lstm':
         return ResnetLSTMSingleOutput
+    elif model_name == 'vgg16_lstm':
+        return Vgg16LSTMSingleOutput
